4cdd2086cc5ba8feb8d35548a29d6f1dc5387311
Jamin <chenxm35@gmail.com>
2014-07-22 13:17:17 +0800

    Update java apidoc.

--- README.md
+++
@@ -1,2 +0,0 @@
-PiggyBox Java Documentation
-================
--- src/test/java/com/piggybox/test/TestInternetDomain.java
+++
@@ -1,9 +0,0 @@
-package com.piggybox.test;
-
-import com.google.common.net.InternetDomainName;
-
-public class TestInternetDomain {
-	public static void main(String[] args){
-		System.out.println(InternetDomainName.from("baidu.com").isTopPrivateDomain());
-	}
-}
